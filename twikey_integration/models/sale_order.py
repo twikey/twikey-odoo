cac0a052e5c9fa4a6df523753412479db6fbcc4b
Koen Serry <482723+koen-serry@users.noreply.github.com>
2023-05-02 14:12:55 +0200

    Corrected sales_order to invoice

--- twikey_integration/models/sale_order.py
+++ twikey_integration/models/sale_order.py
@@ -6,7 +6,10 @@ class SaleOrder(models.Model):
 
     def _prepare_invoice(self):
         res = super(SaleOrder, self)._prepare_invoice()
-        if "template_id" in self._context:
-            res["template_id"] = self._context.get("template_id")
-
+        if "twikey_template_id" in self._context:
+            res["twikey_template_id"] = self._context.get("twikey_template_id")
+        if "default_send_to_twikey" in self._context:
+            res["send_to_twikey"] = self._context.get("default_send_to_twikey")
+        if "default_auto_collect_invoice" in self._context:
+            res["auto_collect_invoice"] = self._context.get("default_auto_collect_invoice")
         return res
