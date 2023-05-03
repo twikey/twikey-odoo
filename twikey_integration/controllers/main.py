0e9ad7d6b17912261e7864e48ae9a97da14de38b
Koen Serry <482723+koen-serry@users.noreply.github.com>
2023-05-03 18:32:34 +0200

    Corrected sudo for webhook

--- twikey_integration/controllers/main.py
+++ twikey_integration/controllers/main.py
@@ -22,26 +22,32 @@ class TwikeyController(http.Controller):
         received_sign = request.httprequest.headers.get('X-Signature')
         api_key = request.env['ir.config_parameter'].get_twikey_api_key()
         if not Webhook.verify_signature(payload, received_sign, api_key):
+            _logger.warning("Twikey: failed signature verification %s", pprint.pformat(post))
             return Response(status=403)
+
+        if post.get("msg") == "dummytest":
+            _logger.info("Twikey Webhook test successfull!")
+            return "OK"
+
         _logger.info('Twikey: entering form_feedback with post data %s', pprint.pformat(post))
         if post and post.get('mandateNumber'):
             # See https://www.twikey.com/api/#webhooks
             if post.get('type') == 'contract':
                 # Removal of a prepared mandate doesn't show up in the feed
                 if post.get('event') == 'Invite' and post.get('reason') == 'removed':
-                    mandate_id = request.env['mandate.details'].search([('reference', '=', post.get('mandateNumber'))])
+                    mandate_id = request.env['mandate.details'].sudo().search([('reference', '=', post.get('mandateNumber'))])
                     if mandate_id:
-                        mandate_id.with_context(update_feed=True).unlink()
+                        mandate_id.with_context(update_feed=True).sudo().unlink()
                 elif post.get('event') == 'Sign':
-                    mandate_id = request.env['mandate.details'].search([('reference', '=', post.get('mandateNumber'))])
+                    mandate_id = request.env['mandate.details'].sudo().search([('reference', '=', post.get('mandateNumber'))])
                     if mandate_id:
-                        mandate_id.with_context(update_feed=True).write({'state': 'signed'})
+                        mandate_id.with_context(update_feed=True).sudo().write({'state': 'signed'})
                 else:
                     mandate_obj = request.env['mandate.details']
-                    mandate_obj.update_feed()
+                    mandate_obj.sudo().update_feed()
             elif post.get('type') == 'payment':
                 invoice_obj = request.env['account.invoice']
-                invoice_obj.update_invoice_feed()
+                invoice_obj.sudo().update_invoice_feed()
                 return "OK"
         else:
             return "OK"
