d11617a324501b5114f173e2e90ffe76082348ae
Koen Serry <482723+koen-serry@users.noreply.github.com>
2023-10-12 23:12:44 +0200

    Corrected setting behaviour for multicompany

--- payment_twikey/models/res_config_settings.py
+++ payment_twikey/models/res_config_settings.py
@@ -19,6 +19,17 @@ class ResConfigSettings(models.TransientModel):
     twikey_include_purchase = fields.Boolean(string="Send purchase invoices", related="company_id.twikey_include_purchase", readonly=False)
     twikey_send_pdf = fields.Boolean(string="Include PDF", related="company_id.twikey_send_pdf", readonly=False)
 
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        company = self.env.company.sudo()
+        res.update(
+            twikey_send_invoice= company.twikey_send_invoice,
+            twikey_auto_collect= company.twikey_auto_collect,
+            twikey_include_purchase= company.twikey_include_purchase,
+            twikey_send_pdf= company.twikey_send_pdf,
+        )
+        return res
+
     def twikey_refresh_credentials(self):
         try:
             twikey_client = self.env["ir.config_parameter"].get_twikey_client(company=self.env.company)
